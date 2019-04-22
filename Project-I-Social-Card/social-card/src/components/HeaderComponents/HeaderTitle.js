901a9a938ec9b403b1374d1acc060f1783b6ef2c
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-04-22 15:27:56 -0500

    working on header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+
+
+const HeaderTitle = () => {
+  return (
+    <div className="header">
+      <h3>Lambda School</h3>
+      <h4>@LambdaSchool * 26 jan </h4>
+    </div>
+  )
+}
+export default HeaderTitle;
